8753bd2d3b72e399b0ecd6f3efcb288c96571824
stefaneidelloth <eis@isi.fhg.de>
2024-05-02 12:21:49 +0200

    trying default config #4

--- renovate-config.js
+++ renovate-config.js
@@ -9,51 +9,7 @@
 // https://docs.renovatebot.com/self-hosted-configuration/
 
 module.exports = {
-  "username": "renovate-release",
-  "gitAuthor": "Renovate Bot <bot@renovateapp.com>",
-  "onboarding": false,
-  "requireConfig": "optional",
-  "platform": "github",
-  "forkProcessing": "enabled",
-  "dryRun": null, //"full",  // use full to only log messages instead of creating pull requests
-  "autodiscover": true,
-  "packageRules": [
-    {
-     "description": "lockFileMaintenance",
-     "matchUpdateTypes": [
-       "pin",
-       "digest",
-       "patch",
-       "minor",
-       "major",
-       "lockFileMaintenance"
-     ],
-     "automerge": true,
-     "automergeType": "branch",
-     "ignoreTests": true, // set to false if you want updates only to be installed if tests pass
-     "dependencyDashboardApproval": false,
-     "minimumReleaseAge": null
-    },
-    {
-      // eslint 9 is not supported by some packages, yet, for example
-      // https://github.com/typescript-eslint/typescript-eslint/issues/8211
-      // https://github.com/eslint-recommended/eslint-config/issues/5
-      "matchPackageNames": ["eslint"],
-      "allowedVersions": "8.56.0"
-    },
-    {
-      "matchPackageNames": ["eslint-plugin-n"],
-      "allowedVersions": "16.6.2"
-    },
-    {
-      // eslint-recommended/eslint-config@16.0.3 requires eslint-plugin-unicorn@"48.0.0"
-      "matchPackageNames": ["eslint-plugin-unicorn"],
-      "allowedVersions": "48.0.0"
-    },
-    {
-      // eslint-recommended/eslint-config@16.0.3 requires  eslint-plugin-jsdoc@"^46.0.0"
-      "matchPackageNames": ["eslint-plugin-jsdoc"],
-      "allowedVersions": "46.9.1"
-    }
-  ]
-};
+  "$schema": "https://docs.renovatebot.com/renovate-schema.json",
+  "extends": ["config:recommended"]
+}
+
